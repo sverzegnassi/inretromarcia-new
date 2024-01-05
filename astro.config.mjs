cb87551e7d33a4b600570748848044c3b444e9bd
Stefano Verzegnassi <stefano@sverzegnassi.me>
2024-01-05 17:42:28 +0100

    Add keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react(), markdoc(), keystatic()],
+  output: 'hybrid',
 });
