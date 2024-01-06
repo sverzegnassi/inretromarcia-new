9f164be10974b5a564b096c82298395d677fc936
Stefano Verzegnassi <stefano@sverzegnassi.me>
2024-01-06 19:26:52 +0100

    Config adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from '@keystatic/astro';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), markdoc(), keystatic()],
   output: 'hybrid',
+  adapter: netlify(),
 });
