06b963fd55b9978231844f43e6b0f821ffb58b4d
Stefano Verzegnassi <stefano@sverzegnassi.me>
2024-01-07 08:38:09 +0100

    Moved editor components UI in a separate file

--- src/keystatic/editor/components/UploadComponent.ts
+++ src/keystatic/editor/components/UploadComponent.ts
@@ -1,60 +1,21 @@
-import { NotEditable, component, fields } from "@keystatic/core";
+import { component, fields } from "@keystatic/core";
 import { fileIcon } from '@keystar/ui/icon/icons/fileIcon';
 import { filePlus2Icon } from '@keystar/ui/icon/icons/filePlus2Icon';
-import { css, tokenSchema } from '@keystar/ui/style';
-import React, { type ReactElement } from "react";
+import React from "react";
+import { KeystarIcon, LabelWithCaption, LayoutH } from "./ui-common";
 
-function KeystarIcon(icon: ReactElement) {
-    return React.cloneElement(icon, {
-        className: css({
-            width: tokenSchema.size.icon.large,
-            height: tokenSchema.size.icon.large,
-            fill: "transparent",
-            stroke: tokenSchema.color.foreground.neutral,
-        })
-    })
-}
-
-function Layout(children: ReactElement[]) {
-    return React.createElement(NotEditable, {
-        className: css({
-            display: 'flex',
-            alignItems: 'center',
-            columnGap: tokenSchema.size.space.medium
-        })
-    }, children);
-}
-
-function LabelWithCaption(label: string, caption: string) {
-    return React.createElement('div', {
-        className: css({
-            display: 'flex',
-            flexDirection: 'column',
-            rowGap: tokenSchema.size.space.xsmall,
-        })
-    }, [
-        React.createElement('span', {
-            className: css({
-                fontSize: tokenSchema.typography.text.small.size,
-                color: tokenSchema.color.foreground.neutralSecondary
-            })
-        }, caption)
-    ],
-        React.createElement('span', null, label)
-    )
-}
 
 export default () => {
     return component({
         preview: ({ fields }) => {
             if (fields.ref.value) {
-                return Layout([
+                return LayoutH([
                     KeystarIcon(fileIcon),
                     LabelWithCaption(fields.ref.value, "Percorso della risorsa selezionata")
                 ]);
             }
 
-            return Layout([
+            return LayoutH([
                 KeystarIcon(filePlus2Icon),
                 React.createElement('span', null, "Seleziona un file da incorporare...")
             ]);
---
+++ src/keystatic/editor/components/ui-common.ts
@@ -0,0 +1,53 @@
+import { NotEditable } from "@keystatic/core";
+import { css, tokenSchema } from '@keystar/ui/style';
+import React, { type ReactElement } from "react";
+
+export const LayoutH = (children: ReactElement[]) => {
+    return React.createElement(NotEditable, {
+        className: css({
+            display: 'flex',
+            alignItems: 'center',
+            columnGap: tokenSchema.size.space.medium
+        })
+    }, children);
+}
+
+export const LayoutV = (children: ReactElement[]) => {
+    return React.createElement(NotEditable, {
+        className: css({
+            display: 'flex',
+            flexDirection: 'column',
+            rowGap: tokenSchema.size.space.medium
+        })
+    }, children);
+}
+
+export const LabelWithCaption = (label: string, caption: string) => {
+    return React.createElement('div', {
+        className: css({
+            display: 'flex',
+            flexDirection: 'column',
+            rowGap: tokenSchema.size.space.xsmall,
+        })
+    }, [
+        React.createElement('span', {
+            className: css({
+                fontSize: tokenSchema.typography.text.small.size,
+                color: tokenSchema.color.foreground.neutralSecondary
+            })
+        }, caption)
+    ],
+        React.createElement('span', null, label)
+    )
+}
+
+export const KeystarIcon = (icon: ReactElement) => {
+    return React.cloneElement(icon, {
+        className: css({
+            width: tokenSchema.size.icon.large,
+            height: tokenSchema.size.icon.large,
+            fill: "transparent",
+            stroke: tokenSchema.color.foreground.neutral,
+        })
+    })
+}
