e3ebb2baf8660b5e128e757a1c6954598ec6d78b
Stefano Verzegnassi <stefano@sverzegnassi.me>
2024-01-08 05:24:04 +0100

    ImageComponent: Implement workaround for Keystatic validation issue.

--- src/keystatic/editor/components/ImageComponent.ts
+++ src/keystatic/editor/components/ImageComponent.ts
@@ -24,7 +24,68 @@ export default () => {
             file: fields.image({
                 label: "Percorso risorsa",
                 directory: "src/assets/uploads",
-                publicPath: "src/assets/uploads/"
+                publicPath: "src/assets/uploads/",
+                
+                /* WORKAROUND: Ensure that the user uploads a valid picture.
+
+                    Explanation:
+                    If the field is left empty due to no image being uploaded, Keystatic fails to create
+                    the `file` attribute in the .mdoc when saving the document.
+
+                    When attempting to access the .mdoc document through the Keystatic UI, the application anticipates
+                    the presence of the `file` attribute.
+
+                    As this attribute is absent, the saved `image` block deviates from the specification outlined
+                    in Keystatic configuration, leading the application to throw a validation error.
+
+                    To help identifying the issue before saving, `fields.image()` is now marked as required.
+
+                    Example of the issue:
+
+                    ```
+                    {% image
+                    title="My Caption"
+                    alt="My alt text"
+                    attribution={} /%}
+                    ```
+
+                    Expected format:
+
+                    ```
+                    {% image
+                    file=""
+                    title="My Caption"
+                    alt="My alt text"
+                    attribution={} /%}
+                    ```
+                */
+                /* UPSTREAM BUG: In the current alpha version of Keystatic, document validation is not fully implemented.
+                    
+                    When the `image` field is left empty, a "Contains invalid fields. Please edit." label appears
+                    in the block preview. This aligns with the intended behavior of `validation: { isRequired: true }`.
+
+                    However:
+
+                    Keystatic does not prevent the user from saving the document in an invalid state.
+                    
+                    The "Save" button remains in an active, hoverable, and pressable state, misleadingly suggesting
+                    that the document is valid and can be successfully saved. Clicking the button triggers 
+                    a validation error (see above). The document is saved in a corrupted state,
+                    and can not be accessed through the GUI, necessitating manual correction in the source code.
+
+                    Desirable fixes:
+
+                    - Clicking the button when the document is in an invalid state should result in an error dialog
+                    shown to the user. The error dialog explains why this is happening and provides indications
+                    to resolve the issue (a simple "Please review the document to identify and address any errors."
+                    would be sufficient).
+
+                    - The "Save" button appears disabled and non-hoverable. Its blue, primary color is de-emphasized or
+                    changed to a semantic color value that indicates "caution" or "critical." Additionally, a warning icon
+                    is displayed inside the button or alongside the button. If displayed alongside the button,
+                    an additional "Check the content for errors" string is visible.
+                */
+                validation: { isRequired: true }
             }),
             title: fields.text({
                 label: 'Titolo / Didascalia',
